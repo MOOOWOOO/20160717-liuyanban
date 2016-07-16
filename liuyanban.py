24f1e51608a95f1e0491a51f242e0819155a8c03
mooowooo <mooowooo@live.com>
2016-07-17 02:28:49 +0800

    format code

--- liuyanban.py
+++ liuyanban.py
@@ -13,17 +13,18 @@ __author__ = 'Jux.Liu'
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def index_view():
-    message_list=Message.query.all()
+    message_list = Message.query.all()
     return render_template("index.html", message_list=message_list)
+
 
 @app.route('/message/add', methods=['POST'])
 def add_message():
-    message=Message(request.form)
+    message = Message(request.form)
     message.save()
     return redirect(url_for('index_view'))
+
 
 if __name__ == '__main__':
     app.run()
